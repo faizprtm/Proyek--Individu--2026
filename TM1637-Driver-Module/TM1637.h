a94f91e7d37ade7490ba7b518eb32a54c0082732
Faiz Faadhil <114850144+faizprtm@users.noreply.github.com>
2026-09-20 11:28:21 +0700

    Add files via upload
    STM32 Header and Source file for TM1637 4-digit 7-segment LED display driver

---
+++ TM1637-Driver-Module/TM1637.h
@@ -0,0 +1,14 @@
+/*
+ * TM1637.h
+ *
+ *  Created on: Sep 13, 2026
+ *      Author: JK-01
+ */
+
+#ifndef INC_TM1637_H_
+#define INC_TM1637_H_
+
+void TM1637_WriteData (uint8_t Addr, uint8_t *data, int size);
+
+
+#endif /* INC_TM1637_H_ */
